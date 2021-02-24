2bf7555cd6294d28d151ca11a5d92c62abcbb7ea
Sophie <sophienanterre@gmail.com>
2021-02-24 15:49:19 +0100

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Sophie Jacquot  on 24/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
